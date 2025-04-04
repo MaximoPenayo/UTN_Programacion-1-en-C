ad19c11d87faf795c53f48c12e76676dae58e8a3
Maximo_Penayo <maximopenayo1@gmail.com>
2025-04-04 00:17:14 -0300

    Actulizado

---
+++ C/Hola Mundo/hola.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+
+
+int main()
+{
+    printf("HOLA MUNDO :D");
+    return 0;
+}
---
+++ C/Ordenar Numeros/OrdenarNumeros.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+//Alumno: Penayo Maximo Joel
+//DNI: 46459945
+//Legajo: 30412
+
+
+int main(){
+	
+	int num1,num2,num3;
+	int aux;
+	
+	
+	printf("Ingrese un numero: ");	
+	scanf("%d",&num1);
+	printf("Ingrese otro numero: ");
+	scanf("%d",&num2);
+	printf("Ingrese otro numero: ");
+	scanf("%d",&num3);
+	
+	if(num1 > num2){
+		aux = num1;
+		num1 = num2;
+		num2 = aux;
+	}if(num1 > num3){
+		aux = num1;
+		num1 = num3;
+		num3 = aux;
+	}if(num2 > num3){
+		aux = num2;
+		num2 = num3;
+		num3 = aux;
+	}
+	
+	printf("\n");
+	printf("-------------------------------------------------------------------------------\n");
+	printf("Los numeros ordenados de mayor a menos son: %d - %d - %d\n",num1,num2,num3);
+	printf("-------------------------------------------------------------------------------\n");
+	
+	
+	return 0;
+}
