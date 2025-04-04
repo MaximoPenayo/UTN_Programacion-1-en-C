255debedb739156508547dba8ba85fa00e128419
Maximo_Penayo <maximopenayo1@gmail.com>
2025-04-04 00:41:39 -0300

    Ahora

---
+++ 9)Edad y Categoria/EdadYCategoria.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*Solicita la edad de una persona y clasifica en:
+Menor de edad (<18 años)
+Adulto (18-64 años)
+Adulto mayor (=65 años)
+*/
+
+int main() {
+	int edad;
+	
+	printf("Ingrese su Edad: ");
+	scanf("%d",&edad);
+	
+	if(edad<18){
+		printf("Usted es un 'Menor de Edad'\n");
+	}
+	if(edad>=18 && edad <=64){
+		printf("Usted es un 'Adulto'\n");
+	}
+	if(edad>=65){
+		printf("Usted es un 'Adulto Mayor'\n");
+	}
+	return 0;
+}
+
